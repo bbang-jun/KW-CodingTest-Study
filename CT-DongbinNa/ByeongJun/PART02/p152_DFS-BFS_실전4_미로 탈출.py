96d637101e23365da2ec20168717ffe5d2c9490e
bbangjun <lucid9dawn@gmail.com>
2023-09-12 17:02:18 +0900

    comment(p152_미로 탈출): 주석 작성 완료

--- CT-DongbinNa/ByeongJun/PART02/p152_DFS-BFS_실전4_미로 탈출.py
+++ CT-DongbinNa/ByeongJun/PART02/p152_DFS-BFS_실전4_미로 탈출.py
@@ -2,14 +2,14 @@ from collections import deque
 
 
 def bfs(maze, N, M, cost):
-    queue = deque([(0, 0)])
-    cost[0][0] = 1
+    queue = deque([(0, 0)]) # 첫 위치를 queue에 넣기
+    cost[0][0] = 1 # 칸을 셀 때는 시작 칸도 포함
 
     while queue:
-        i, j = queue.popleft()
+        i, j = queue.popleft() # queue에서 pop을 하여 인덱스 좌표 얻기
 
-        if maze[i][j] == 1:
-            if i-1 >= 0 and maze[i-1][j] == 1 and cost[i-1][j] == 0:
+        if maze[i][j] == 1: # 괴물이 없는 부분이고, 이동하지 않은 부분이면 queue에 저장 및 이동 칸 수 갱신
+            if i-1 >= 0 and maze[i-1][j] == 1 and cost[i-1][j] == 0: #
                 queue.append((i-1, j))
                 cost[i-1][j] = cost[i][j] + 1
             if i+1 <= N-1 and maze[i+1][j] == 1 and cost[i+1][j] == 0:
@@ -22,7 +22,7 @@ def bfs(maze, N, M, cost):
                 queue.append((i, j+1))
                 cost[i][j+1] = cost[i][j] + 1
 
-    return cost[N-1][M-1]
+    return cost[N-1][M-1] # 마지막 칸에 도착하였으므로 해당 위치에서의 이동 칸 수 반환
 
 N, M = map(int, input().split())
 cost = [[0]*M for _ in range(N)]
