ea1106b56e97c7690bffe004d822c08687ba7fe6
bbangjun <lucid9dawn@gmail.com>
2023-09-12 17:17:39 +0900

    comment(p182_두 배열의 원소 교체): 주석 작성 완료

--- CT-DongbinNa/ByeongJun/PART02/p182_정렬_실전4_두 배열의 원소 교체.py
+++ CT-DongbinNa/ByeongJun/PART02/p182_정렬_실전4_두 배열의 원소 교체.py
@@ -1,16 +1,18 @@
 N, K = map(int, input().split())
 
-result = 0
+result = 0 # 모든 원소의 합 저장을 위한 변수
 
+# 원소의 개수만큼 정적으로 입력받지 않으므로 list화 하여 입력받기
 array_a = list(map(int, input().split()))
 array_b = list(map(int, input().split()))
 
+# 배열 A는 오름차순, 배열 B는 내림차순으로 정렬
 array_a.sort() # 1 2 3 4 5
 array_b.sort(reverse=True) # 6 6 5 5 5
 
-for i in range(K):
+for i in range(K): # 최대 K번 바꿔치기 연산 수행
     if array_a[i] < array_b[i]:
-        array_a[i], array_b[i] = array_b[i], array_a[i]
+        array_a[i], array_b[i] = array_b[i], array_a[i] # 스와핑 수행
 
 for a in array_a:
     result += a
