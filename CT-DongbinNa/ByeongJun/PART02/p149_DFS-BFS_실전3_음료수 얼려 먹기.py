3443eeef3130af846766a0c55fb91fc032b52873
bbangjun <lucid9dawn@gmail.com>
2023-09-12 16:56:22 +0900

    comment(p149_음료수 얼려 먹기): 주석 작성 완료

--- CT-DongbinNa/ByeongJun/PART02/p149_DFS-BFS_실전3_음료수 얼려 먹기.py
+++ CT-DongbinNa/ByeongJun/PART02/p149_DFS-BFS_실전3_음료수 얼려 먹기.py
@@ -3,25 +3,25 @@ def dfs(ice, i, j):
         return False
 
     if ice[i][j] == 0:
-        ice[i][j] = 1
+        ice[i][j] = 1 # 방문 처리
         dfs(ice, i-1, j) # 상
         dfs(ice, i+1, j) # 하
         dfs(ice, i, j-1) # 좌
         dfs(ice, i, j+1) # 우
 
-    return True
+    return True # True 시에 아이스크림 1개 추가
 
 
 N, M = map(int, input().split())
-ice = []
-icecream = 0
+ice = [] # 그래프 역할의 얼음 틀 리스트 변수
+icecream = 0 # 생성되는 아이스크림 개수
 
 for i in range(N):
-    ice.append(list(map(int, input())))
+    ice.append(list(map(int, input()))) # 얼음틀 입력
 
 for i in range(N):
     for j in range(M):
-        if dfs(ice, i, j):
+        if dfs(ice, i, j): # dfs 함수를 실행하여 이동할 수 있는 부분을 다 이동하면 아이스크림 개수 증가
             icecream += 1
 
 print(icecream)
