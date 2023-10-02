fdaebf270dbff2de566392c76d8470382f63a4d1
bbangjun <lucid9dawn@gmail.com>
2023-10-03 02:41:49 +0900

    solved(p313_문자열 뒤집기): 풀이 완료(그리디)

--- CT-DongbinNa/ByeongJun/PART02/p303_그래프이론_실전4_커리큘럼.py
+++ CT-DongbinNa/ByeongJun/PART02/p303_그래프이론_실전4_커리큘럼.py
@@ -1,12 +1,12 @@
 # 선후수 관계가 있으므로 위상 정렬을 사용해야 함
 # 동시에 강의 수강이 가능하므로, 예시에서 1번 강의와 2번 강의를 동시에 수강하고, 3번 강의 수강 시에 최소 70시간
 from collections import deque
-N = int(input()) # N: 강의의 수
 import copy
+N = int(input()) # N: 강의의 수
 
 indegree = [0] * (N + 1)
 time = [0] * (N + 1)
-# time_result = [0] * (N + 1) # 결과
+time_result = [0] * (N + 1) # 결과
 graph = [[] for i in range(N+1)]
 
 for i in range(1, N+1):
@@ -14,14 +14,13 @@ for i in range(1, N+1):
     time[i] = temp[0]
     #for j in range(1, len(temp) + 1):
     for j in temp[1:-1]:
-        #if temp[j] == -1:
-         #   break
         indegree[i] += 1
         graph[j].append(i)
 
 # indegree가 0이면 해당 시간 그대로 출력
 # indegree가 0 이상이면 선수 과목들 중 최대 시간 + 후수 과목 시간
 def topology_sort():
+    global time_result
     time_result = copy.deepcopy(time)
     q = deque()
 
@@ -38,8 +37,7 @@ def topology_sort():
             if indegree[i] == 0:
                 q.append(i)
 
-    for i in time_result[1:len(time_result)+1]:
-        print(i)
-
 topology_sort()
 
+for i in time_result[1:]:
+    print(i)
