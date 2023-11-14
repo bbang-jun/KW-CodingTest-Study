4766afaedf8ae78d578f699a73c3256f371d61a8
bbangjun <lucid9dawn@gmail.com>
2023-11-14 16:33:55 +0900

    solved(353_Q21): 인구 이동(DFS-BFS)

---
+++ CT-DongbinNa/ByeongJun/PART03/p353_DFS-BFS_Q21_인구 이동.py
@@ -0,0 +1,50 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+def solution():
+    n, l, r = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    visited = [[-1] * n for _ in range(n)]
+    cnt = 0
+    move = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    cand = deque([(i, j) for i in range(n) for j in range(i % 2, n, 2)])
+
+    while True:
+        q = deque()
+        for _ in range(len(cand)):
+            i, j = cand.popleft()
+            if visited[i][j] == cnt:
+                continue
+            visited[i][j] = cnt
+            area = set([(i, j)])
+            popul = board[i][j]
+            q.append((i, j))
+
+            while q:
+                x, y = q.popleft()
+                for a, b in move:
+                    dx = x + a;
+                    dy = y + b
+                    if dx >= n or dx < 0 or dy >= n or dy < 0 or visited[dx][dy] == cnt:
+                        continue
+
+                    if l <= abs(board[x][y] - board[dx][dy]) <= r:
+                        visited[dx][dy] = cnt
+                        area.add((dx, dy))
+                        popul += board[dx][dy]
+                        q.append((dx, dy))
+
+            if len(area) > 1:
+                avg_popul = popul // len(area)
+                for x, y in area:
+                    board[x][y] = avg_popul
+                    cand.append((x, y))
+        if cand:
+            cnt += 1
+        else:
+            break
+    print(cnt)
+
+solution()
